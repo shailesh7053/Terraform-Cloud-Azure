260f1d801ea89d92dee24fd4efb95b949a04f17b
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-01-13 21:06:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "storageaccount" {
-  name     = "storage-acc-rg"
+  name     = "storageacc-rg"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = "storage-acc-01"
+  name                     = "storageacc01"
   resource_group_name      = azurerm_resource_group.storageaccount.name
   location                 = azurerm_resource_group.storageaccount.location
   account_tier             = "Standard"
