b4a52de4fb1a21300360b4e8fa2e821151a9c7e9
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-01-13 21:04:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,27 @@
-
-resource "azurerm_resource_group" "StorageAccount" {
-  name     = "storage-acc-01"
-  location = "West Europe"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.57.0"
+    }
+  }
+  required_version = ">1.9.0"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+provider "azurerm" {
+  features{
+  }
+}
+
+resource "azurerm_resource_group" "StorageAccount" {
+  name     = "storage-acc-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "StorageAccount" {
+  name                     = "storage-acc-01"
+  resource_group_name      = azurerm_resource_group.StorageAccount.name
+  location                 = azurerm_resource_group.StorageAccount.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
